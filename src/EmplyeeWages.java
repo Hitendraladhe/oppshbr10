91fcf55f760c6973ba3db80a03d477c23f0e371e
Your Name <Hituladheds@gmail.com>
2024-06-18 17:33:02 +0530

    welcome to Employee wages

--- src/EmplyeeWages.java
+++ src/EmplyeeWages.java
@@ -1,42 +1,57 @@
 import java.util.Random;
-class Company{
 
-}
-class Employee extends Company{
-    boolean isPresent;
-    int wages = 0;
-    int salary;
-    void employeePresentOrNot(int isPresent){
-       if (isPresent >=5){
-           System.out.println("Employee is present!");
-       }
-       else{
-           System.out.println("Employee is absent!");
-       }
+class Employee {
+    private static boolean is_present;
+    private static int total_salary = 0;
+    private String company_name;
+    private static int hour;
+    private static int day;
+    private static int salary;
+    private static boolean is_part_time = false;
+
+    public Employee(String company, int hours, int days) {
+        this.company_name = company;
+        this.hour = hours;
+        this.day = days;
+        System.out.println("Welcome to the "+company_name+" company.... ");
+        calculateMonthlyWages();
     }
-    void dailyWages(int hour ){
-        wages += hour * 8;
-        System.out.println("yours to days wages is " + wages);
-    }
-    void monthlyWages(int hour, int days ){
-        salary += hour * days;
-        System.out.println("yours to days wages is " + salary);
-    }
-    void monthlyCondWages(int hour, int days ){
-        if(hour >= 1000 && days>=20){
-            salary += hour * days;
-            System.out.println("yours to days wages is " + salary);
+
+    public static void  calculateMonthlyWages() {
+        Random rand = new Random();
+        int num = rand.nextInt(10);
+        if(num >= 5){
+            System.out.println("Employee is present! ");
+        }else{
+            System.out.println("Employee is absent! ");
+        }
+
+        switch (hour) {
+            case (4):
+                is_part_time = true;
+                System.out.println("The Employee is part time ");
+            case(8):
+                System.out.println("The Employee is full time ");
+            default:
+                System.out.println("The Employee is full time ");
+        }
+        total_salary += hour * 8;
+        System.out.println("yours to days wages is " + total_salary);
+         switch (hour) {
+            case 100:
+                salary += hour * day;
+                System.out.println("the total days wages is " + salary);
         }
     }
 }
 
-public class EmplyeeWages {
+class EmplyeeWages {
     public static void main(String[] args) {
-        Random rand = new Random();
-        int num = rand.nextInt(10);
-        System.out.println("Welcome to the office....");
-        Employee obj = new Employee();
-        obj.employeePresentOrNot(num);
+        Employee obj1 = new Employee("Reliance", 100, 20);
+        Employee obj2 = new Employee("Tata", 80, 15);
 
     }
 }
+
+
+
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,98 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        int [] array = {1, 2, 3, 4 ,56, 7, 7};
+        int len = array.length;
+        for (int i=0; i<len; i++){
+            for (int j =i; j< len; j++){
+                if(array[i] < array[j]){
+                    int y = array[i];
+                    array[i] = array[j];
+                    array[j] = y;
+                }
+            }
         }
+        System.out.println(Arrays.toString(array));
     }
 }
+
+
+
+
+
+// ArmStrong number
+//Scanner sc = new Scanner(System.in);
+//        int num = sc.nextInt();
+//int du = num;
+//int numdigit = Integer.toString(num).length();
+//int multi = 0;
+//        while(du>0){
+//int digit =  du % 10;
+////System.out.println(digit);
+//multi += Math.pow(digit, numdigit);
+//du/=10;
+//        }
+//        if(multi == num){
+//        System.out.println("number is armstrong num");
+//        }
+//                else{
+//                System.out.println("number is not a armstrong num");
+//        }
+
+
+
+//String len = String.valueOf(num);
+//char[] charArray =  len.toCharArray() ;
+//int a = charArray.length-1;
+//String Array = "";
+//String du ;
+//        for(int i=a; i>=0; i--){
+//        System.out.println(charArray[i]);
+//Array = Array + charArray[i];
+//        }
+//        if (Integer.parseInt(Array) == num){
+//        System.out.println("this num is palindrome ");
+//        }
+//                else{
+//                System.out.println("this num is not palindrome ");
+//        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
+///boolean mybool = false;
+//byte mybyte = 10;
+//short myShort = 10;
+//long myLong = 12334345L;
+//float myFloat = 12.4F;
+//double mydouble = 12.33;
+//char myChar = 'A';
+//int a = 5; // binary: 101
+//int b = 3; // binary: 011
+//int bitwiseAnd = a & b; // 1 (binary: 001)
+//int bitwiseOr = a | b; // 7 (binary: 111)
+//int bitwiseXor = a ^ b; // 6 (binary: 110)
+//        System.out.println(bitwiseAnd+" "+ bitwiseOr+" "+ bitwiseXor);
+//String my = "Hitendra";
+//        System.out.println(mybool);
+//        System.out.println(mybyte);
+//        System.out.println(myShort);
+//        System.out.println(myLong);
+//        System.out.println(myFloat);
+//        System.out.println(mydouble);
+//        System.out.println(myChar);
+//        System.out.println(my);
